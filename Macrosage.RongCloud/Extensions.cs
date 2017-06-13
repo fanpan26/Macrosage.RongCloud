f24bc4a12621091a77c5aa1f4aedec1fe176d3a5
Fanyuepan <fanpan26@126.com>
2017-06-13 11:28:10 +0800

    add tojson method

--- Macrosage.RongCloud/Extensions.cs
+++ Macrosage.RongCloud/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,16 @@ namespace Macrosage.RongCloud
 {
     internal static class Extensions
     {
+        private static readonly IRestRequest request = new RestRequest();
         public static long ToTimestamp(this DateTime time)
         {
             DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
             return (long)(time - startTime).TotalSeconds;
+        }
+
+        public static string ToJson(this object value)
+        {
+            return request.JsonSerializer.Serialize(value);
         }
     }
 }
