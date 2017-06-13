1f5ccdf4dbd651b7540c1bdcb19bb4feb0cb1b2a
Fanyuepan <fanpan26@126.com>
2017-06-13 12:04:38 +0800

    add some apis

--- Macrosage.RongCloud/RongCloudApi.cs
+++ Macrosage.RongCloud/RongCloudApi.cs
@@ -21,5 +21,15 @@ namespace Macrosage.RongCloud
         /// 发送聊天消息接口
         /// </summary>
         public static readonly string Api_SendChatRoomMessage = "/message/chatroom/publish.json";
+
+        /// <summary>
+        /// 刷新用户信息
+        /// </summary>
+        public static readonly string Api_RefreshUser = "/user/refresh";
+
+        /// <summary>
+        /// 检测用户是否在线
+        /// </summary>
+        public static readonly string Api_CheckUserOnline = "/user/checkOnline";
     }
 }
